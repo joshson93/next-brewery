eed2f609bad488e94324e37bdca17961dc863766
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-07-29 13:15:39 +0100

    changed url

--- components/BreweryTable/src/constants.ts
+++ components/BreweryTable/src/constants.ts
@@ -2,4 +2,4 @@ export const PAGE_HEADER = 'Table of all breweries';
 
 export const COLUMN_NAMES = ['Name', 'Type', 'Address', 'Website'];
 
-export const BASE_URL = 'http://localhost:3000/api/breweries';
+export const BASE_URL = 'https://next-brewery.vercel.app/api/breweries';
